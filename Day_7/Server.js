039787aafe45a0f6ef367d4f1bb0886bb1d67eaa
Adarsh Kumar <adarshbhardwaj157@gmail.com>
2025-09-11 22:56:32 +0530

    Add files via upload

---
+++ Day_7/Server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World . This is my program ');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
